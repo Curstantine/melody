6fe0465c0353bb657b2711a1556ce92157527f25
Rachala Ovin <resistlyfe@gmail.com>
2023-08-14 15:43:17 +0530

    fix: neutralino build patches

--- app/plugins/authInfo.ts
+++ app/plugins/authInfo.ts
@@ -11,13 +11,19 @@ export default function authInfoPlugin(): Plugin {
 	const virtualModuleId = "virtual:auth-info";
 	const resolvedVirtualModuleId = "\0" + virtualModuleId;
 
+	let mode: "serve" | "build";
+
 	return {
 		name: "authInfoInject",
+		configResolved(x) {
+			mode = x.command;
+		},
 		resolveId: (id) => {
 			if (id === virtualModuleId) return resolvedVirtualModuleId;
 		},
 		load: (id) => {
 			if (id !== resolvedVirtualModuleId) return;
+			if (mode !== "serve") return `export default {}`;
 
 			const authInfoJson = readFileSync(pathResolve("..", ".tmp", "auth_info.json"));
 			const conf: AuthInfoData = JSON.parse(authInfoJson.toString());
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,22 @@
 import { filesystem } from "@neutralinojs/lib";
 import "./App.css";
+import { createSignal, For } from "solid-js";
 
 function App() {
+	const [files, setFiles] = createSignal<string[]>([]);
+
 	filesystem.readDirectory("./")
-		.then(console.log)
+		.then((x) => setFiles(x.map((y) => y.entry)))
 		.catch(console.error);
 
 	return (
 		<>
 			<h1>Lol</h1>
+			<ul>
+				<For each={files()}>
+					{(item) => <li>{item}</li>}
+				</For>
+			</ul>
 		</>
 	);
 }
