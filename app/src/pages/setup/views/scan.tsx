050b3d4c36bdf9e9cd2f1d010828ab5186b62a87
Curstantine <curstantine@gmail.com>
2023-09-23 17:36:45 +0530

    feat: scanning view

--- app/src/pages/setup/views/scan.tsx
+++ app/src/pages/setup/views/scan.tsx
@@ -1,6 +1,7 @@
 import { onMount, Show } from "solid-js";
 
 import CircularLoader from "@/components/Loader/Circular";
+
 import { useSetupView } from "@/pages/setup/index.model";
 import SetupScanViewModel from "@/pages/setup/views/scan.model";
 
@@ -12,17 +13,32 @@ export default function SetupScanView() {
 
 	onMount(() => {
 		if (!pageData) throw new Error("No page data");
-		viewModel.startScan(pageData.name, pageData.scanLocations);
+		setTimeout(() => viewModel.startScan(pageData.name, pageData.scanLocations), 1000);
 	});
 
 	return (
-		<div class="flex flex-col items-center justify-center">
+		<div
+			class="max-h-md max-w-lg w-full flex flex-col overflow-auto rounded"
+			classList={{
+				"items-center": payload() === null,
+				"b-(1 border-secondary) bg-background-secondary p-4": payload() !== null,
+			}}
+		>
 			<Show when={payload()} fallback={<CircularLoader />}>
 				{(payload) => (
 					<>
-						<span>{payload().action_type === "reading" ? "Reading" : "Indexing"}</span>
-						<span>{payload().path}</span>
-						<span>({payload().current}/{payload().total})</span>
+						<span class="text-2xl leading-tight font-orbiter-display text-text-1">
+							Scanning your library
+						</span>
+						<span class="leading-tight font-orbiter-text text-text-2">
+							This action might take some time...
+						</span>
+
+						<div class="mt-8 flex gap-1 text-text-2">
+							<span>{payload().action_type === "reading" ? "Reading" : "Indexing"}</span>
+							<span>({payload().current}/{payload().total})</span>
+						</div>
+						<span class="text-sm text-text-3">{payload().path}</span>
 					</>
 				)}
 			</Show>
