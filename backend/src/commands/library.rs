6e99fc9a5ebbeccb14e22384a731658b5c264e8c
Curstantine <curstantine@gmail.com>
2023-10-31 10:48:27 +0530

    chore: change backend run config

--- backend/src/commands/library.rs
+++ backend/src/commands/library.rs
@@ -37,7 +37,7 @@ pub async fn get_library_names(app_state: tauri::State<'_, AppState>) -> Result<
 }
 
 #[tauri::command]
-#[tracing::instrument(skip(window, app_state), err(Debug))]
+#[tracing::instrument(skip_all, err(Debug))]
 pub async fn create_library(
 	name: String,
 	scan_locations: Vec<String>,
