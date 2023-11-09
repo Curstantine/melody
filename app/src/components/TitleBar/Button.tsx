370a17cbe234509fe351ed264ef2b706473cd907
Curstantine <curstantine@gmail.com>
2023-11-09 19:53:57 +0530

    feat: focus support for the titlebar and refactor titlebar button

--- app/src/components/TitleBar/Button.tsx
+++ app/src/components/TitleBar/Button.tsx
@@ -14,17 +14,14 @@ export default function TitleBarButton(_props: Props) {
 
 	return (
 		<button
-			class="h-full w-12 inline-flex items-center justify-center text-text-2 transition-colors duration-200"
+			onClick={(e) => props.onClick?.call(null, e)}
+			class="h-full w-12 inline-flex items-center justify-center transition-colors use-transition-standard"
 			classList={{
 				[styles.button_common]: props.type === "common",
 				[styles.button_close]: props.type === "close",
 			}}
-			onClick={props.onClick}
 		>
-			<span
-				class="h-[16px] w-[16px]"
-				classList={{ [props.icon]: true }}
-			/>
+			<span class="h-[16px] w-[16px]" classList={{ [props.icon]: true }} />
 		</button>
 	);
 }
--- app/src/components/TitleBar/index.tsx
+++ app/src/components/TitleBar/index.tsx
@@ -1,37 +1,55 @@
 import { appWindow } from "@tauri-apps/api/window";
-import { createSignal, onMount } from "solid-js";
+import { createSignal, onCleanup, onMount } from "solid-js";
 import { Portal } from "solid-js/web";
 
 import TitleBarButton from "@/components/TitleBar/Button";
+import Result from "@/utils/result";
 
 export default function TitleBar() {
 	const [isMaximized, setMaximizeStatus] = createSignal(false);
+	const [isFocused, setFocusStatus] = createSignal(false);
 
 	let draggableRef: HTMLDivElement | undefined;
+	const listeners: Array<() => void> = [];
 
-	onMount(() => {
-		appWindow.isMaximized()
-			.then((maximized) => setMaximizeStatus(maximized))
-			.catch((e) => console.error(e));
+	const checkMaximizeStatus = async () => {
+		const isMax = await Result.runAsync(appWindow.isMaximized);
+		if (isMax.isOk()) {
+			setMaximizeStatus(isMax.unwrap());
+		}
+	};
+
+	const toggleMaximize = async () => await appWindow.toggleMaximize();
+	const minimize = async () => await appWindow.minimize();
+	const close = async () => await appWindow.close();
+
+	onMount(async () => {
+		await checkMaximizeStatus();
+
+		const isFocus = await Result.runAsync(appWindow.isFocused);
+		if (isFocus.isOk()) {
+			setFocusStatus(isFocus.unwrap());
+		}
+
+		const maximizeListener = await appWindow.onResized(async () => await checkMaximizeStatus());
+		const focusListener = await appWindow.onFocusChanged(({ payload }) => setFocusStatus(payload));
+
+		listeners.push(maximizeListener, focusListener);
 	});
 
-	const toggleMaximize = async () => {
-		await appWindow.toggleMaximize();
-	};
-
-	const minimize = async () => {
-		await appWindow.minimize();
-	};
-
-	const close = async () => {
-		await appWindow.close();
-	};
+	onCleanup(() => listeners.forEach((unlisten) => unlisten()));
 
 	return (
 		<Portal mount={document.getElementById("titlebar")!}>
-			<div class="inset-x-0 h-8 flex items-center justify-between border-b-1 border-b-border-main border-b-solid">
+			<div
+				class="inset-x-0 h-8 flex items-center justify-between border-b-1 border-b-border-main border-b-solid"
+				classList={{
+					"bg-background-secondary text-text-3": !isFocused(),
+					"bg-background-main text-text-2": isFocused(),
+				}}
+			>
 				<div data-tauri-drag-region ref={draggableRef!} class="h-full inline-flex flex-1 items-center">
-					<span class="select-none pl-4 text-sm font-orbiter-deck-medium text-text-2">Melody</span>
+					<span class="select-none pl-4 text-sm font-orbiter-deck-medium">Melody</span>
 				</div>
 				<div class="h-full inline-flex">
 					<TitleBarButton icon="i-mdi-minimize" onClick={minimize} />
