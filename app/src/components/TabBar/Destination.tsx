0a9b541cec5654b98d49b361e206df82958d60da
Curstantine <curstantine@gmail.com>
2023-11-14 19:52:23 +0530

    feat: basic dnd sort functionality

--- app/src/components/TabBar/Destination.tsx
+++ app/src/components/TabBar/Destination.tsx
@@ -1,14 +1,45 @@
 import { Link } from "@solidjs/router";
+import { createSignal, type JSX } from "solid-js";
 
+type DragEventParam = Parameters<JSX.EventHandler<HTMLDivElement, DragEvent>>[0];
+export type DragEventFunc = (id: string, e: DragEventParam) => void;
 export type Props = {
-    href: string;
-    label: string;
-}
+	id: string;
+	href: string;
+	label: string;
+	onDragStart: DragEventFunc;
+	onDragEnd: DragEventFunc;
+};
 
 export default function TabBarDestination(props: Props) {
-    return (
-        <Link href={props.href} class="text-text-2 use-transition-standard hover:text-text-1" activeClass="text-text-1">
-            {props.label}
-        </Link>
-    )
+	const [isFloating, setFloatStatus] = createSignal(false);
+
+	const onDragStart: JSX.EventHandler<HTMLDivElement, DragEvent> = (e) => {
+		setFloatStatus(true);
+		props.onDragStart.call(null, props.id, e);
+	};
+
+	const onDragEnd: JSX.EventHandler<HTMLDivElement, DragEvent> = (e) => {
+		setFloatStatus(false);
+		props.onDragEnd.call(null, props.id, e);
+	};
+
+	return (
+		<div
+			id={props.id}
+			draggable={true}
+			onDragStart={onDragStart}
+			onDragEnd={onDragEnd}
+			class="transition-transform use-transition-standard"
+			classList={{ "cursor-move": isFloating() }}
+		>
+			<Link
+				href={props.href}
+				class="text-text-2 use-transition-standard hover:text-text-1"
+				activeClass="text-text-1"
+			>
+				{props.label}
+			</Link>
+		</div>
+	);
 }
--- app/src/components/TabBar/index.tsx
+++ app/src/components/TabBar/index.tsx
@@ -1,20 +1,84 @@
 import { createSignal, For } from "solid-js";
 
-import TabBarDestination, { type Props as Destination } from "@/components/TabBar/Destination";
+import TabBarDestination, { DragEventFunc, type Props as Destination } from "@/components/TabBar/Destination";
 
 export default function TabBar() {
-	const [destinations] = createSignal<Destination[]>([
-		{ label: "Albums", href: "/(shared)/albums" },
-		{ label: "Artists", href: "/(shared)/albums" },
-		{ label: "Playlists", href: "/(shared)/playlists" },
-		{ label: "Now Playing", href: "/(shared)/np" },
+	// eslint-disable-next-line prefer-const
+	let destinationListRef: HTMLDivElement | undefined = undefined;
+
+	const [destinations, setDestinations] = createSignal<Omit<Destination, "onDragStart" | "onDragEnd">[]>([
+		{ id: "music", label: "Music", href: "/(shared)/music" },
+		{ id: "artists", label: "Artists", href: "/(shared)/artists" },
+		{ id: "playlists", label: "Playlists", href: "/(shared)/playlists" },
+		{ id: "np", label: "Now Playing", href: "/(shared)/np" },
 	]);
+
+	const onDestinationDrag: DragEventFunc = (id, e) => {};
+
+	/**
+	 * Thinking that the destinations are an array with a predefined index to each position.
+	 * If we were to listen to the drag event and deduce the relative X width the dragging element
+	 * and each element on the list has, we can
+	 */
+	const onDestinationDrop: DragEventFunc = (id, e) => {
+		if (!destinationListRef) return;
+
+		const items = destinations();
+		const draggedIndex = items.findIndex(item => item.id === id);
+		let droppedIndex = draggedIndex;
+
+		const rectCache: DOMRect[] = [];
+		for (let i = 0; i < destinationListRef.children.length; i++) {
+			const isLast = i === destinationListRef.children.length - 1;
+
+			const child = destinationListRef.children[i];
+			const childRect = rectCache[i] ?? child.getBoundingClientRect();
+
+			const nextChild = isLast ? child : destinationListRef.children[i + 1]!;
+			const nextChildRect = nextChild.getBoundingClientRect();
+			if (!isLast) rectCache[i + 1] = nextChildRect;
+
+			console.log("element at", child.id, {
+				childId: child.id,
+				childLeft: childRect.left,
+				childRight: childRect.right,
+				nextChildId: nextChild.id,
+				nextChildLeft: nextChildRect.left,
+				nextChildRight: nextChildRect.right,
+			});
+
+			// Cases where drag to right is bigger than the childRect end.
+			if (i === destinationListRef.children.length - 1 && e.clientX >= childRect.right) {
+				droppedIndex = i;
+				break;
+			}
+
+			if (e.clientX > childRect.left && e.clientX < nextChildRect.left) {
+				droppedIndex = i;
+				break;
+			}
+		}
+
+		const newItems = [...items];
+		const itemAtDraggedIndex = newItems[draggedIndex];
+		newItems[draggedIndex] = newItems[droppedIndex];
+		newItems[droppedIndex] = itemAtDraggedIndex;
+
+		setDestinations(newItems);
+	};
 
 	return (
 		<div class="h-8 flex items-center border-b-1 border-b-border-main border-b-solid px-4">
 			<div class="w-16" />
-			<div class="inline-flex items-center gap-4 text-sm text-text-2">
-				<For each={destinations()}>{(x) => <TabBarDestination {...x} />}</For>
+			<div
+				ref={destinationListRef}
+				class="inline-flex items-center gap-4 text-sm text-text-2"
+				onDragOver={(e) => e.preventDefault()}
+				onDragEnter={(e) => e.preventDefault()}
+			>
+				<For each={destinations()}>
+					{(x) => <TabBarDestination {...x} onDragStart={onDestinationDrag} onDragEnd={onDestinationDrop} />}
+				</For>
 			</div>
 		</div>
 	);
