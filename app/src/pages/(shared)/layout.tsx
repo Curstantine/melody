b1cc0c2882c48ef7e4aecf8f2b1d71bac1be6840
Curstantine <curstantine@gmail.com>
2023-11-23 21:09:56 +0530

    feat: sizing algorithm for items in /music

--- app/src/pages/(shared)/layout.tsx
+++ app/src/pages/(shared)/layout.tsx
@@ -4,9 +4,9 @@ import TabBar from "@/components/TabBar";
 
 export default function SharedLayout() {
 	return (
-		<div class="flex flex-col">
+		<>
 			<TabBar />
 			<Outlet />
-		</div>
+		</>
 	);
 }
--- app/src/pages/(shared)/music/index.tsx
+++ app/src/pages/(shared)/music/index.tsx
@@ -1,7 +1,38 @@
+import { appWindow } from "@tauri-apps/api/window";
+import { createSignal, For, onCleanup, onMount } from "solid-js";
+
 export default function Home() {
+	const [gridXSize, setGridXSize] = createSignal(4);
+
+	let ref: HTMLDivElement;
+	const listeners: Array<() => void> = [];
+
+	onMount(async () => {
+		const remConst = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+		const resizeListener = await appWindow.onResized(() => {
+			const { width } = ref.getBoundingClientRect();
+			const widthRem = width / remConst;
+
+			// 10.5 item width + 1 gap
+			const itemLength = widthRem / 11.5;
+			setGridXSize(Math.floor(itemLength));
+		});
+
+		listeners.push(resizeListener);
+	});
+
+	onCleanup(() => listeners.forEach((fn) => fn()));
+
 	return (
-		<div>
-			home
+		<div
+			ref={ref!}
+			style={`--grid-x-size: repeat(${gridXSize()}, minmax(0, 1fr));`}
+			class="grid grid-cols-[var(--grid-x-size)] h-full items-center justify-center gap-4 overflow-y-auto p-4"
+		>
+			<For each={Array.from({ length: 50 })}>
+				{(_) => <div class="h-42 w-42 rounded bg-background-secondary"></div>}
+			</For>
 		</div>
 	);
 }
