ed7d2373c7187224be08640730e607908e9f7b1f
Curstantine <curstantine@gmail.com>
2023-11-29 14:26:11 +0530

    fix: add directories to init app state

--- backend/src/models/state.rs
+++ backend/src/models/state.rs
@@ -16,6 +16,8 @@ impl AppState {
 	pub async fn initialize(&self, app_handle: &tauri::AppHandle) -> Result<()> {
 		let directories = Directories::new(app_handle).await?;
 		let db = Database::new(&directories.database_dir).await?;
+
+		self.directories.lock().await.replace(directories);
 		self.db.lock().await.replace(db);
 
 		let mut init = self.initialized.lock().unwrap();
