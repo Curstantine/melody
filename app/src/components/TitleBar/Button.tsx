e0b2b62b8bda7a3a97d330884b722a77e76635d1
Curstantine <curstantine@gmail.com>
2023-12-12 18:34:13 +0530

    fix: corrupted styles from dprint run in titlebar

--- app/src/components/TitleBar/Button.tsx
+++ app/src/components/TitleBar/Button.tsx
@@ -1,12 +1,15 @@
 import { mergeProps } from "solid-js";
 import type { JSX } from "solid-js/jsx-runtime";
 
-import styles from "./styles/button.module.css";
-
 type Props = {
 	icon: string;
 	type?: "common" | "close";
 	onClick?: JSX.EventHandler<HTMLButtonElement, MouseEvent>;
+};
+
+const styles = {
+	common: "hover:bg-titlebar-button-common-background-hover active:bg-titlebar-button-common-background-active",
+	close: "hover:bg-titlebar-button-close-background-hover active:bg-titlebar-button-close-background-active",
 };
 
 export default function TitleBarButton(_props: Props) {
@@ -15,10 +18,10 @@ export default function TitleBarButton(_props: Props) {
 	return (
 		<button
 			onClick={(e) => props.onClick?.call(null, e)}
-			class="h-full w-12 inline-flex items-center justify-center transition-colors use-transition-standard"
+			class="h-full w-12 inline-flex items-center justify-center bg-transparent transition-colors use-transition-standard"
 			classList={{
-				[styles.button_common]: props.type === "common",
-				[styles.button_close]: props.type === "close",
+				[styles.common]: props.type === "common",
+				[styles.close]: props.type === "close",
 			}}
 		>
 			<span class="h-[16px] w-[16px]" classList={{ [props.icon]: true }} />
